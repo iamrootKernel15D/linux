26b21478d70a01eecd0c2d4aa3d4c311dc5444f6
Sorinara <lowid86@gmail.com>
2018-10-31 17:24:08 +0900

    2018-10-27 study comment

--- arch/x86/boot/main.c
+++ arch/x86/boot/main.c
@@ -16,10 +16,12 @@
 #include "boot.h"
 #include "string.h"
 
+//메모리 주소를 16의 배수로 설정.
+//ex) int a 의 주소값은 0016 or 0032 or 0048 ... 0096 
 struct boot_params boot_params __attribute__((aligned(16)));
 
 char *HEAP = _end;
-char *heap_end = _end;		/* Default end of heap = no heap */
+char *heap_end = _end;        /* Default end of heap = no heap */
 
 /*
  * Copy the header into the boot parameter block.  Since this
@@ -29,32 +31,40 @@ char *heap_end = _end;		/* Default end of heap = no heap */
 
 static void copy_boot_params(void)
 {
-	struct old_cmdline {
-		u16 cl_magic;
-		u16 cl_offset;
-	};
-	const struct old_cmdline * const oldcmd =
-		(const struct old_cmdline *)OLD_CL_ADDRESS;
+    struct old_cmdline {
+        u16 cl_magic;
+        u16 cl_offset;
+    };
+    
+    //OLD_CL_ADDRESS = 0x020 == 32
+    const struct old_cmdline * const oldcmd =
+        (const struct old_cmdline *)OLD_CL_ADDRESS;
 
-	BUILD_BUG_ON(sizeof boot_params != 4096);
-	memcpy(&boot_params.hdr, &hdr, sizeof hdr);
+    BUILD_BUG_ON(sizeof boot_params != 4096);
+    /*
+     setup_header는 header.S 의 
+        hdr: 라벨에서부터 header 섹션이 끝날때까지의 데이터
+     boot.h : extern struct setup_header hdr;     
+    */
+    memcpy(&boot_params.hdr, &hdr, sizeof hdr);
 
-	if (!boot_params.hdr.cmd_line_ptr &&
-	    oldcmd->cl_magic == OLD_CL_MAGIC) {
-		/* Old-style command line protocol. */
-		u16 cmdline_seg;
+    //cmd_line_ptr 초기값은 0 이다.
+    if ( !boot_params.hdr.cmd_line_ptr &&
+        oldcmd->cl_magic == OLD_CL_MAGIC) {
+        /* Old-style command line protocol. */
+        u16 cmdline_seg;
 
-		/* Figure out if the command line falls in the region
-		   of memory that an old kernel would have copied up
-		   to 0x90000... */
-		if (oldcmd->cl_offset < boot_params.hdr.setup_move_size)
-			cmdline_seg = ds();
-		else
-			cmdline_seg = 0x9000;
+        /* Figure out if the command line falls in the region
+           of memory that an old kernel would have copied up
+           to 0x90000... */
+        if (oldcmd->cl_offset < boot_params.hdr.setup_move_size)
+            cmdline_seg = ds();
+        else
+            cmdline_seg = 0x9000;
 
-		boot_params.hdr.cmd_line_ptr =
-			(cmdline_seg << 4) + oldcmd->cl_offset;
-	}
+        boot_params.hdr.cmd_line_ptr =
+            (cmdline_seg << 4) + oldcmd->cl_offset;
+    }
 }
 
 /*
@@ -64,15 +74,15 @@ static void copy_boot_params(void)
  */
 static void keyboard_init(void)
 {
-	struct biosregs ireg, oreg;
-	initregs(&ireg);
+    struct biosregs ireg, oreg;
+    initregs(&ireg);
 
-	ireg.ah = 0x02;		/* Get keyboard status */
-	intcall(0x16, &ireg, &oreg);
-	boot_params.kbd_status = oreg.al;
+    ireg.ah = 0x02;        /* Get keyboard status */
+    intcall(0x16, &ireg, &oreg);
+    boot_params.kbd_status = oreg.al;
 
-	ireg.ax = 0x0305;	/* Set keyboard repeat rate */
-	intcall(0x16, &ireg, NULL);
+    ireg.ax = 0x0305;    /* Set keyboard repeat rate */
+    intcall(0x16, &ireg, NULL);
 }
 
 /*
@@ -80,22 +90,22 @@ static void keyboard_init(void)
  */
 static void query_ist(void)
 {
-	struct biosregs ireg, oreg;
+    struct biosregs ireg, oreg;
 
-	/* Some older BIOSes apparently crash on this call, so filter
-	   it from machines too old to have SpeedStep at all. */
-	if (cpu.level < 6)
-		return;
+    /* Some older BIOSes apparently crash on this call, so filter
+       it from machines too old to have SpeedStep at all. */
+    if (cpu.level < 6)
+        return;
 
-	initregs(&ireg);
-	ireg.ax  = 0xe980;	 /* IST Support */
-	ireg.edx = 0x47534943;	 /* Request value */
-	intcall(0x15, &ireg, &oreg);
+    initregs(&ireg);
+    ireg.ax  = 0xe980;     /* IST Support */
+    ireg.edx = 0x47534943;     /* Request value */
+    intcall(0x15, &ireg, &oreg);
 
-	boot_params.ist_info.signature  = oreg.eax;
-	boot_params.ist_info.command    = oreg.ebx;
-	boot_params.ist_info.event      = oreg.ecx;
-	boot_params.ist_info.perf_level = oreg.edx;
+    boot_params.ist_info.signature  = oreg.eax;
+    boot_params.ist_info.command    = oreg.ebx;
+    boot_params.ist_info.event      = oreg.ecx;
+    boot_params.ist_info.perf_level = oreg.edx;
 }
 
 /*
@@ -104,79 +114,79 @@ static void query_ist(void)
 static void set_bios_mode(void)
 {
 #ifdef CONFIG_X86_64
-	struct biosregs ireg;
+    struct biosregs ireg;
 
-	initregs(&ireg);
-	ireg.ax = 0xec00;
-	ireg.bx = 2;
-	intcall(0x15, &ireg, NULL);
+    initregs(&ireg);
+    ireg.ax = 0xec00;
+    ireg.bx = 2;
+    intcall(0x15, &ireg, NULL);
 #endif
 }
 
 static void init_heap(void)
 {
-	char *stack_end;
+    char *stack_end;
 
-	if (boot_params.hdr.loadflags & CAN_USE_HEAP) {
-		asm("leal %P1(%%esp),%0"
-		    : "=r" (stack_end) : "i" (-STACK_SIZE));
+    if (boot_params.hdr.loadflags & CAN_USE_HEAP) {
+        asm("leal %P1(%%esp),%0"
+            : "=r" (stack_end) : "i" (-STACK_SIZE));
 
-		heap_end = (char *)
-			((size_t)boot_params.hdr.heap_end_ptr + 0x200);
-		if (heap_end > stack_end)
-			heap_end = stack_end;
-	} else {
-		/* Boot protocol 2.00 only, no heap available */
-		puts("WARNING: Ancient bootloader, some functionality "
-		     "may be limited!\n");
-	}
+        heap_end = (char *)
+            ((size_t)boot_params.hdr.heap_end_ptr + 0x200);
+        if (heap_end > stack_end)
+            heap_end = stack_end;
+    } else {
+        /* Boot protocol 2.00 only, no heap available */
+        puts("WARNING: Ancient bootloader, some functionality "
+             "may be limited!\n");
+    }
 }
 
 void main(void)
 {
-	/* First, copy the boot header into the "zeropage" */
-	copy_boot_params();
+    /* First, copy the boot header into the "zeropage" */
+    copy_boot_params();
 
-	/* Initialize the early-boot console */
-	console_init();
-	if (cmdline_find_option_bool("debug"))
-		puts("early console in setup code\n");
+    /* Initialize the early-boot console */
+    console_init();
+    if (cmdline_find_option_bool("debug"))
+        puts("early console in setup code\n");
 
-	/* End of heap check */
-	init_heap();
+    /* End of heap check */
+    init_heap();
 
-	/* Make sure we have all the proper CPU support */
-	if (validate_cpu()) {
-		puts("Unable to boot - please use a kernel appropriate "
-		     "for your CPU.\n");
-		die();
-	}
+    /* Make sure we have all the proper CPU support */
+    if (validate_cpu()) {
+        puts("Unable to boot - please use a kernel appropriate "
+             "for your CPU.\n");
+        die();
+    }
 
-	/* Tell the BIOS what CPU mode we intend to run in. */
-	set_bios_mode();
+    /* Tell the BIOS what CPU mode we intend to run in. */
+    set_bios_mode();
 
-	/* Detect memory layout */
-	detect_memory();
+    /* Detect memory layout */
+    detect_memory();
 
-	/* Set keyboard repeat rate (why?) and query the lock flags */
-	keyboard_init();
+    /* Set keyboard repeat rate (why?) and query the lock flags */
+    keyboard_init();
 
-	/* Query Intel SpeedStep (IST) information */
-	query_ist();
+    /* Query Intel SpeedStep (IST) information */
+    query_ist();
 
-	/* Query APM information */
+    /* Query APM information */
 #if defined(CONFIG_APM) || defined(CONFIG_APM_MODULE)
-	query_apm_bios();
+    query_apm_bios();
 #endif
 
-	/* Query EDD information */
+    /* Query EDD information */
 #if defined(CONFIG_EDD) || defined(CONFIG_EDD_MODULE)
-	query_edd();
+    query_edd();
 #endif
 
-	/* Set the video mode */
-	set_video();
+    /* Set the video mode */
+    set_video();
 
-	/* Do the last things and invoke protected mode */
-	go_to_protected_mode();
+    /* Do the last things and invoke protected mode */
+    go_to_protected_mode();
 }
